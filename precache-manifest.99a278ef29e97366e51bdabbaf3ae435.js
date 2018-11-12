46d19df6b39f5b65d93738a9baf46fd3e5b1d30b
rayz2007 <rayz2007@uw.edu>
2018-11-12 15:43:20 -0800

    Updates

---
+++ precache-manifest.99a278ef29e97366e51bdabbaf3ae435.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-practice/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f878aa0a124d9b346b05",
+    "url": "/react-practice/static/js/runtime~main.f878aa0a.js"
+  },
+  {
+    "revision": "81e03feb06762f5ed9aa",
+    "url": "/react-practice/static/js/main.81e03feb.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/react-practice/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "81e03feb06762f5ed9aa",
+    "url": "/react-practice/static/css/main.feccaeae.chunk.css"
+  },
+  {
+    "revision": "69ff6016a31dfc3171aa7f693798dff7",
+    "url": "/react-practice/index.html"
+  }
+];
