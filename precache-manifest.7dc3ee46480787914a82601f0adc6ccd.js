42767cdc009aa38ca3c912c6decacf680e9f10ba
rayz2007 <rayz2007@uw.edu>
2018-11-12 20:34:45 -0800

    Updates

---
+++ precache-manifest.7dc3ee46480787914a82601f0adc6ccd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "49a6dc2995e4f5d092b38d04f729992a",
+    "url": "/react-practice/static/media/raycsv.49a6dc29.csv"
+  },
+  {
+    "revision": "f878aa0a124d9b346b05",
+    "url": "/react-practice/static/js/runtime~main.f878aa0a.js"
+  },
+  {
+    "revision": "c196971277b696f17e83",
+    "url": "/react-practice/static/js/main.c1969712.chunk.js"
+  },
+  {
+    "revision": "71a50e43d8cd7f826888",
+    "url": "/react-practice/static/js/1.71a50e43.chunk.js"
+  },
+  {
+    "revision": "c196971277b696f17e83",
+    "url": "/react-practice/static/css/main.597cb6cb.chunk.css"
+  },
+  {
+    "revision": "e34f9284c0422805685e0da9565f7920",
+    "url": "/react-practice/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-practice/precache-manifest.88724e08200ff426ac79f740c55bec41.js"
+  "/react-practice/precache-manifest.7dc3ee46480787914a82601f0adc6ccd.js"
 );
 
 workbox.clientsClaim();
